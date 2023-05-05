b612753e1e3f6755168d531a6bce5ca74f29324e
NicoAG12 <agueronico247@gmail.com>
2023-05-04 21:55:04 -0300

    Se agrego validación de formulario registro y local storage

---
+++ scriptAdmin.js
@@ -0,0 +1,6 @@
+
+const form = document.getElementById("formAñadirJuego")
+const codigoJuego=document.getElementById("codigoJuego")
+const nombreJuego = document.getElementById("nombreJuego")
+const descripcionJuego=document.getElementById("descripcionJuego")
+const buttonAdd = document.getElementById("buttonAdd")
---
+++ scriptRegistro.js
@@ -0,0 +1,44 @@
+
+
+const registerButton = document.getElementById("registerButton")
+
+const usuario = [
+    {
+        user_name: "admin",
+        user_password: "admin",
+        user_email : "admin",
+    }
+]
+
+localStorage.setItem("userList",JSON.stringify(usuario));
+
+registerButton.addEventListener("click", (e) =>{
+    e.preventDefault();
+
+    const userRegister = {
+        user_name : document.getElementById("userName").value,
+        user_password : document.getElementById("userPassword").value,
+        user_email : document.getElementById("userMail").value,
+    }
+
+        if (JSON.parse(localStorage.getItem("userList").includes(userRegister.user_name)) || JSON.parse(localStorage.getItem("userList").includes(userRegister.user_email))  ){
+            alert("Usuario o mail existentes, ingrese nuevamente")
+        } else{
+            usuario.push(userRegister);
+            localStorage.setItem("userList",JSON.stringify(usuario));
+            alert("Registro completado!")
+        }
+    
+/*
+    else{
+        usuario.push(userRegister);
+
+      localStorage.setItem("userList",JSON.stringify(usuario));
+    }
+    
+*/
+
+})
+
+
+
